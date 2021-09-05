617ed03900591b164269569fdec7054b60b0f802
antikostya <prototype3628800@yandex.ru>
2021-09-05 13:42:23 +0300

    email fix

---
+++ email-fix.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch -f --env-filter '
+OLD_EMAIL="kostya@hp.com"
+CORRECT_NAME="antikostya"
+CORRECT_EMAIL="prototype3628800@yandex.ru"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
